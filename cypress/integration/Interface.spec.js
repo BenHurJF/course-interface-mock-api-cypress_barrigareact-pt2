2db0442db4cdea370cf909702f3d9229c050fe01
BenHur JF <beiujeffer@hotmail.com>
2021-11-01 02:54:47 -0300

    Impedimento ao MOCKAR LOGIN pois mudou os comandos :(

--- cypress/integration/Interface.spec.js
+++ cypress/integration/Interface.spec.js
@@ -1,22 +1,34 @@
 /// <reference types="Cypress" />
+import { useFakeXMLHttpRequest } from 'cypress/types/sinon';
 import loc from '../support/locators';
+
+// const res = [ 
+//     {
+//     id : 25541,
+//     nome: "Ben-Hur Jeffer",
+//     token: "String grande mockada para simular o token"
+//     }
+// ]
 
 describe('Cenários de teste', function () {
     before(() => {
-        cy.log('Pré condição: Login')
-        cy.LOGIN(Cypress.env('user_email'), Cypress.env('user_senha'))
+        // cy.LOGIN(Cypress.env('user_email'), Cypress.env('user_senha')
+        // cy.intercept('POST', '/signin', { 
+        //     response: res
+        // })
+        cy.LOGIN(Cypress.env('user_email'), Cypress.env('senha errada'))
         cy.ResetApp()
     })
     beforeEach(() => {
         cy.get(loc.MENU.HOME).click()
-})
+    })
 
     it('1- Inserindo Conta', () => {
         cy.get(`a[class='nav-link dropdown-toggle']`).click()
         cy.get('[href="/contas"]').click()
         cy.get('input[data-test="nome"]').type('create account')
         cy.get('.btn').click()
-        cy.get('.toast-message', {timeout:22000}).should('contain', 'Conta inserida com sucesso')
+        cy.get('.toast-message', { timeout: 22000 }).should('contain', 'Conta inserida com sucesso')
     })
 
     it('2 - Alterando a conta', () => {
@@ -25,7 +37,7 @@ describe('Cenários de teste', function () {
         cy.xpath(loc.ALTERAR.FN_XP_ALTERCONTA('Conta para saldo')).click()
         cy.get('input[data-test="nome"]').clear().type('Conta para saldo 1')
         cy.get('.btn').click()
-        cy.get('.toast-message', {timeout:22000}).should('contain', 'Conta atualizada com sucesso!')
+        cy.get('.toast-message', { timeout: 22000 }).should('contain', 'Conta atualizada com sucesso!')
     })
 
     it('3 - Tentar criar conta já existente', () => {
@@ -33,7 +45,7 @@ describe('Cenários de teste', function () {
         cy.get('[href="/contas"]').click()
         cy.get('input[data-test="nome"]').type('Conta para movimentacoes')
         cy.get('.btn').click()
-        cy.get('.toast-message', {timeout:22000}).should('contain', 'Erro: Error: Request failed with status code 400')
+        cy.get('.toast-message', { timeout: 22000 }).should('contain', 'Erro: Error: Request failed with status code 400')
     })
 
     it('4 - Inserir movimentação/Transação', () => {
@@ -44,23 +56,23 @@ describe('Cenários de teste', function () {
         cy.get(loc.MOVIMENTACAO.CONTA).select('Conta para movimentacoes')
         cy.get('[data-test=status]').click()
         cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click()
-        cy.get('.toast-message', {timeout:22000}).should('contain', 'Movimentação inserida com sucesso!')
-        cy.xpath(loc.EXTRATO.FN_XP_ASSERT_DESC('Descrição para TESTE E2E'), {timeout:22000}).should('exist')
-        cy.xpath(loc.EXTRATO.FN_XP_ASSERT_VALUE('333,99'), {timeout:22000}).should('exist')
+        cy.get('.toast-message', { timeout: 22000 }).should('contain', 'Movimentação inserida com sucesso!')
+        cy.xpath(loc.EXTRATO.FN_XP_ASSERT_DESC('Descrição para TESTE E2E'), { timeout: 22000 }).should('exist')
+        cy.xpath(loc.EXTRATO.FN_XP_ASSERT_VALUE('333,99'), { timeout: 22000 }).should('exist')
         cy.xpath('//li//div[@class="col col-md-1"]//i[@class="far fa-trash-alt"]')
     })
 
     it('5 - Consultar Saldo', () => {
         cy.get(loc.SALDO.HOME).click()
         cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Conta para saldo')).should('contain', '534,00')
-        
+
         cy.get('[data-test=menu-extrato]').click()
         cy.xpath(loc.EXTRATO.FN_XP_ALTERAR_ELEMENTO('Movimentacao 1, calculo saldo')).click()
         // cy.wait(2000)
         cy.get(loc.MOVIMENTACAO.DESCRICAO).should('have.value', 'Movimentacao 1, calculo saldo')
-        cy.get('[data-test=status]', {timeout:10000}).click()
+        cy.get('[data-test=status]', { timeout: 10000 }).click()
         cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click()
-        cy.get('.toast-message', {timeout:22000}).should('contain', 'Movimentação alterada com sucesso!')
+        cy.get('.toast-message', { timeout: 22000 }).should('contain', 'Movimentação alterada com sucesso!')
 
         cy.get(loc.SALDO.HOME).click()
         cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Conta para saldo')).should('contain', '4.034,00')
